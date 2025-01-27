d47f11018f4fdf5a7ca40d8bdbeeea0a1265e28c
taoreta <nora.castroeduardo@gmail.com>
2025-01-27 17:26:19 -0600

    Set up Express server with rate limiter, URL routes, and error handler middleware

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,13 @@
 const express = require('express');
 const app = express();
+const limiter = require('./middleware/rateLimiter');
+const urlRoutes = require('./routes/urlRoutes');
+const errorHandler = require('./middleware/errorHandler');
+
+app.use(express.json());
+app.use(limiter); //Limitar las peticiones a la API
+app.use('/api', urlRoutes);
+app.use(errorHandler); //Manejar errores
 
 app.get('/', (req, res) =>{
     res.send("El backend funciona correctamente");
@@ -9,3 +17,4 @@ const PORT = process.env.PORT || 3001;
 app.listen(PORT, ()=> {
     console.log(`Servidor en http://localhost:${PORT}`)
 });
+
